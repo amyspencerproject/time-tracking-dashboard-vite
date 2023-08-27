e4d387482078859e5652d7f4547fa8dbc4cb0f52
amyspencerproject <amy@spencerproject.com>
2023-08-27 12:34:41 -0600

    Add all cards to jsx and add hover states

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,30 @@ function App() {
             </div>
           </div>
           <div className="flex-wrapper frequency-text">
-            <p>Daily</p>
+            <p className="active">Daily</p>
             <p className="active">Weekly</p>
-            <p>Monthly</p>
+            <p className="active">Monthly</p>
           </div>
         </div>
+
+        {/* report cards start */}
         <div className="report-card card work">
-          {/* <img className="report-icon" src="/icon-work.svg" alt="" /> */}
           <div className="report-info card">
             <div className="report-header">
               <h2>Work</h2>
               <div className="svg-wrapper">
-                <img src="/icon-ellipsis.svg" alt="" />
+                <svg
+                  className="svg-ellipsis"
+                  width="21"
+                  height="5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill=""
+                    fillRule="evenodd"
+                  />
+                </svg>
               </div>
             </div>
             <div className="flex-wrapper hours-text">
@@ -37,6 +49,149 @@ function App() {
             </div>
           </div>
         </div>
+
+        <div className="report-card card play">
+          <div className="report-info card">
+            <div className="report-header">
+              <h2>Play</h2>
+              <div className="svg-wrapper">
+                <svg
+                  className="svg-ellipsis"
+                  width="21"
+                  height="5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill=""
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="flex-wrapper hours-text">
+              <span className="total-hours">32hrs</span>
+              <p>
+                Last Week - <span className="previous">36hrs</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="report-card card study">
+          <div className="report-info card">
+            <div className="report-header">
+              <h2>Study</h2>
+              <div className="svg-wrapper">
+                <svg
+                  className="svg-ellipsis"
+                  width="21"
+                  height="5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill=""
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="flex-wrapper hours-text">
+              <span className="total-hours">32hrs</span>
+              <p>
+                Last Week - <span className="previous">36hrs</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="report-card card exercise">
+          <div className="report-info card">
+            <div className="report-header">
+              <h2>Exercise</h2>
+              <div className="svg-wrapper">
+                <svg
+                  className="svg-ellipsis"
+                  width="21"
+                  height="5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill=""
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="flex-wrapper hours-text">
+              <span className="total-hours">32hrs</span>
+              <p>
+                Last Week - <span className="previous">36hrs</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="report-card card social">
+          <div className="report-info card">
+            <div className="report-header">
+              <h2>Social</h2>
+              <div className="svg-wrapper">
+                <svg
+                  className="svg-ellipsis"
+                  width="21"
+                  height="5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill=""
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="flex-wrapper hours-text">
+              <span className="total-hours">32hrs</span>
+              <p>
+                Last Week - <span className="previous">36hrs</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="report-card card self-care">
+          <div className="report-info card">
+            <div className="report-header">
+              <h2>Self Care</h2>
+              <div className="svg-wrapper">
+                <svg
+                  className="svg-ellipsis"
+                  width="21"
+                  height="5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                    fill=""
+                    fillRule="evenodd"
+                  />
+                </svg>
+                {/* <img className="svg-ellipsis" src="/icon-ellipsis.svg" alt="" /> */}
+              </div>
+            </div>
+            <div className="flex-wrapper hours-text">
+              <span className="total-hours">32hrs</span>
+              <p>
+                Last Week - <span className="previous">36hrs</span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* report cards start */}
       </main>
       <footer>
         <p className="attribution">
